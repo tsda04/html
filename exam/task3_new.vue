fabe68c1bb9c31de101118692d260a321dc1fffc
dariatsyganova <tsda04@mail.ru>
2025-05-28 20:21:38 +0400

    upd task3

---
+++ exam/task3_new.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="color-boxes-container">
+    <div
+      v-for="(color, index) in colors"
+      :key="index"
+      class="color-box"
+      :style="{ backgroundColor: color }"
+      @click="changeColor(index)"
+    ></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ColorBoxes',
+  data() {
+    return {
+      colors: ['red', 'green', 'blue', 'yellow', 'purple']
+    }
+  },
+  methods: {
+    changeColor(index) {
+      this.colors[index] = this.getRandomColor()
+    },
+    getRandomColor() {
+      const letters = '0123456789ABCDEF'
+      let color = '#'
+      for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)]
+      }
+      return color
+    }
+  }
+}
+</script>
+
+<style scoped>
+.color-boxes-container {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 10px;
+  padding: 20px;
+}
+
+.color-box {
+  width: 100px;
+  height: 100px;
+  cursor: pointer;
+  transition: background-color 0.3s ease;
+}
+</style>
